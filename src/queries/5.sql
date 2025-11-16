02f7ee1e9274fb02a06097ebb6f0a2e10233940a
Blockky <blockygh6@hotmail.com>
2025-11-16 17:26:58 +0100

    Queries borradas

--- src/queries/5.sql
+++
@@ -1,5 +0,0 @@
-/* 5. Vista donde, para cada temporada, se muestren los pilotos que han corrido en la
-misma, así como los puntos totales que han obtenido cada uno en esa temporada. */
-\pset pager off
-
-\echo 'Temporadas\n'
